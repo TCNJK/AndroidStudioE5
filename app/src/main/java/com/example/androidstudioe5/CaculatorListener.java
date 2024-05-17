9f4541d55e24f7d9b7a1f0279855d7bc5e435fa5
Hồng Lĩnh <101921217+TCNJK@users.noreply.github.com>
2024-05-17 21:07:32 +0700

    add calc use listener and update function caculator

--- app/src/main/java/com/example/androidstudioe5/CaculatorListener.java
+++ app/src/main/java/com/example/androidstudioe5/CaculatorListener.java
@@ -12,17 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CaculatorOnClick extends AppCompatActivity {
+public class CaculatorListener extends AppCompatActivity {
 
     EditText num1;
     EditText num2;
     TextView result;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_caculator_on_click);
+        setContentView(R.layout.activity_caculator_listener);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,19 +31,46 @@ public class CaculatorOnClick extends AppCompatActivity {
         num1 = findViewById(R.id.number1);
         num2 = findViewById(R.id.number2);
         result = findViewById(R.id.result);
+
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caculator('+');
+            }
+        });
+
+        findViewById(R.id.sub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caculator('-');
+            }
+        });
+
+        findViewById(R.id.mul).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caculator('*');
+            }
+        });
+
+        findViewById(R.id.div).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caculator('/');
+            }
+        });
     }
 
-    public void addNumber(View view){
-        caculator('+');
-    }
-    public void subNumber(View view){
-        caculator('-');
-    }
-    public void mulNumber(View view){
-        caculator('*');
-    }
-    public void divNumber(View view){
-        caculator('/');
+    private void caculator(char type){
+        Double number1 = checkNumberValid(num1.getText().toString());
+        Double number2 = checkNumberValid(num2.getText().toString());
+
+        if (number1 == null || number2 == null){
+            result.setText("Invalid input");
+            return;
+        }
+        Double value = calculator(number1, number2, type);
+        result.setText(String.format("%.3f", value));
     }
 
     private Double checkNumberValid(String numberString){
@@ -67,16 +93,5 @@ public class CaculatorOnClick extends AppCompatActivity {
             case '/': return number1/number2;
         }
         return 0.0;
-    }
-    private void caculator(char type){
-        Double number1 = checkNumberValid(num1.getText().toString());
-        Double number2 = checkNumberValid(num2.getText().toString());
-
-        if (number1 == null || number2 == null){
-            result.setText("Invalid input");
-            return;
-        }
-        Double value = calculator(number1, number2, type);
-        result.setText(String.format("%.3f",value));
     }
 }
